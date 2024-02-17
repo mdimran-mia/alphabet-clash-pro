e4820b2225dbe34232bb5c9bf347cdd6dc0d0ecb
mdimran-mia <mdimranmia.info@gmail.com>
2024-02-17 20:47:36 +0600

    added part 1

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,30 +1,45 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
 
-function getARandomAlphabet(){
-    const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetStrings.split('');
-
-    const getARandomNumber = Math.random()*25;
-    const index = Math.round(getARandomNumber);
-
-    const alphabet = alphabets[index];
-    return alphabet;
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
 }
 
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+
 function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
 
-function hideElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('hidden');
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
 }
 
-function showElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden');
-}
+// function getARandomAlphabet() {
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     // get a random index between 0 -25
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+    
+//     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
